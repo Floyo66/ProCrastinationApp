cdebe52e76139ece37a394106529c23ea86c6aba
Alpersue98 <Alperstc98@outlook.de>
2022-11-21 13:23:27 +0100

    Navigation Fix
    user is now able to switch between the navbar during the break time of the timer and also switching back to the break time again.

--- ProCrastinationApp/Assets/Scripts/Navigation.cs
+++ ProCrastinationApp/Assets/Scripts/Navigation.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Navigation : MonoBehaviour
 {
   
-
+    
    [SerializeField] GameObject[] panels;
 
    public void NavigationBarClick(GameObject activePanel){
-        for(int i = 0; i < panels.Length; i++){
+        /* for(int i = 0; i < panels.Length; i++){
             panels[i].SetActive(false);
         }
-        activePanel.SetActive(true);
-        activePanel.SetActive(true);
+        activePanel.SetActive(true); */
+        //activePanel.SetActive(true);
+        
    }
 
 }
