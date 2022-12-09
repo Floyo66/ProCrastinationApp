3c5e98af162ac8384a41694b68c8c35220a01a56
Alpersue98 <Alperstc98@outlook.de>
2022-12-09 13:55:51 +0100

    Auto Login + i really don't know

--- ProCrastinationApp/Assets/Scripts/FirebaseInit.cs
+++ ProCrastinationApp/Assets/Scripts/FirebaseInit.cs
@@ -5,10 +5,77 @@ using Firebase;
 using Firebase.Analytics;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class FirebaseInit : MonoBehaviour
 {
-    public UnityEvent OnFirebaseInit = new UnityEvent();
+
+
+
+
+ public static bool firebaseReady;
+
+    void Start()
+    {
+        CheckIfReady();
+    }
+
+    void Update()
+    {
+        if(firebaseReady == true)
+        {
+            SceneManager.LoadScene("FirebaseLogin");
+        }
+    }
+
+    public static void CheckIfReady()
+    {
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            Firebase.DependencyStatus dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+
+                Firebase.FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
+                firebaseReady = true;
+                Debug.Log("Firebase is ready for use.");
+            }
+            else
+            {
+                firebaseReady = false;
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*     public UnityEvent OnFirebaseInit = new UnityEvent();
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +88,6 @@ public class FirebaseInit : MonoBehaviour
         });
         OnFirebaseInit.Invoke();
         FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
-    }
+    } */
 
 }
